c1ae9d8e863d1e24f7d7ab20f1fb5741f1653b3e
GEOVANY ROJO <70170972+jogerona20@users.noreply.github.com>
2025-02-24 14:45:40 -0700

    commit inicial

---
+++ backend/autentificacion/src/main/java/com/rojo/autentificacion/AutentificacionApplication.java
@@ -0,0 +1,13 @@
+package com.rojo.autentificacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutentificacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutentificacionApplication.class, args);
+	}
+
+}
---
+++ backend/autentificacion/src/test/java/com/rojo/autentificacion/AutentificacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rojo.autentificacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutentificacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
